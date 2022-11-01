4e94c7a9b328247a067c708403bb7d863192b8d0
Kamran Hussein <chinterswill@gmail.com>
2022-11-01 11:25:32 -0400

    commit tree 1848d563d56c5e21161c1cbec8057c1b4b7ba13c

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph")
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+
+
+function appendCat(name) {
+const allCats = [...cats, "Broom"]
+return allCats
+}
+
+function prependCat(name) {
+const allCats = ["Arnold", ...cats]
+return allCats
+}
+
+function removeLastCat() {
+    const lastCat = cats.slice(0, 2)
+    return lastCat
+}
+
+function removeFirstCat() {
+    const firstCat = cats.slice(1)
+    return firstCat
+}
+
+
+console.log(1 + 2 + "puppies" + 4 + 5)
+
+
+
+
